630ab7fb001b7486a0a281efea35f21cfde4dbb7
Aurthur Musendame <aurthurmusendame@gmail.com>
2023-07-15 17:34:11 +0200

    upgraded core packages to latest

--- felicity/apps/multiplex/microbiology/models.py
+++ felicity/apps/multiplex/microbiology/models.py
@@ -1,24 +1,35 @@
+"""Micro Organism Category
+   Gram Reaction and Morpholoy
+"""
+
 """Micro Organism"""
 
-"""Micro Organism Category"""
+"""Micro Organism properties
+   generic props
+   culture media props
+"""
+
+"""Antibiotics Categories"""
 
 """Antibiotics"""
 
-"""Antibiotics Categories"""
 
 """AST Panels and Panel Groups/Types
    organism panel e.g Staphylococcus Drugs
    gram reaction panel e.g Gram Positive Drugs  
    sample type panel e.g Urine Drugs
-   diagnosis panel e.g Burns Drugs
+   diagnosis/condition panel e.g Burns Drugs
 """
 
 """Panel Category e.g By Orgnism, By sample Type, ..."""
 
 """ 
-Gram Reaction
-Culture Appearance
+Culture Appearance ?? require templates
 Micro organisms isolated
-AST per isolate ? support CLSI requirements
 Reflexes if necessary
+"""
+
+"""Reference standard breakpoints
+   CLSI
+   EUCAST
 """
