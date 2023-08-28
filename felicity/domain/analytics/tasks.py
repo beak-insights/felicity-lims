4d98eec2960132c7d9346bad014472eaa659bbca
Aurthur Musendame <aurthurmusendame@gmail.com>
2023-08-28 21:58:09 +0200

    refactored inventory domain

--- felicity/domain/analytics/tasks.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-from apps.notification.utils import FelicityStreamer, ReportNotifier
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-report_notifier = ReportNotifier()
-streamer = FelicityStreamer()
