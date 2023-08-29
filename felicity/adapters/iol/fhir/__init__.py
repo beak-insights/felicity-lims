648e1430ccdba36d60be68f5d8618db727fb8b1b
Aurthur Musendame <aurthurmusendame@gmail.com>
2023-08-29 17:37:10 +0200

    removed iol adapter since its insode the domain

--- felicity/adapters/iol/fhir/__init__.py
+++
@@ -1,5 +0,0 @@
-# https://www.hl7.org/fhir/workflow-communications.html
-# https://wiki.openmrs.org/display/projects/Task+Resource
-# http://docs.openelis-global.org/en/latest/deployomrs/
-# https://wiki.openmrs.org/display/projects/ServiceRequest+Resource
-# http://docs.openelis-global.org/en/latest/messages/
