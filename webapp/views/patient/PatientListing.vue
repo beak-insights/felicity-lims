b75e460734c48d08cefeddb639e218addda61dbd
Aurthur Musendame <musendamea@nmrl.org.zw>
2025-05-09 13:43:55 +0200

    fixed patients register incorrect route

--- webapp/views/patient/PatientListing.vue
+++ webapp/views/patient/PatientListing.vue
@@ -197,7 +197,7 @@ let getPatientFullName = (pt: PatientType) => {
             </div>
             <div class="flex justify-start">
               <RouterLink 
-                to="/patient/register" 
+                to="/patients/register" 
                 class="px-4 py-2 bg-primary text-primary-foreground rounded-md hover:bg-primary/90 transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-primary/50"
               >
                 Register New Patient
